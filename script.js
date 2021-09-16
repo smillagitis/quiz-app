e88306291e0c101a8cea634d3c7eedf4d481b02a
VeronikaH <v.s.khmarova@gmail.com>
2021-09-16 14:11:45 +0500

    almost functional

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const quiz = document.querySelector('.quiz')
 const startScreen = document.querySelector('.start-screen')
 const questionField = document.querySelector('.question')
 const answersField = document.querySelector('.answers')
-const API_URL = "https://opentdb.com/api.php?amount=15&category=15&difficulty=easy&type=multiple"
-let incorrectAnswers = [];
-let correctAnswer = '';
+//const nextButton = document.querySelector('#next-button')
+const resultsScreen = document.querySelector('.results-screen')
+const API_URL = "https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple"
+let correctAnswer;
 let currentQuestionIndex = 0;
-let rightAnswersCount;
+let rightAnswersCount = 0;
 startButton.addEventListener('click', startQuiz)
+//nextButton.addEventListener('click', getNextQuestion)
 
 function startQuiz() {
     quiz.style.display = "block";
@@ -28,18 +30,35 @@ async function getQuizData() {
 async function renderQuiz() {
     let quizData = await getQuizData();
     quizData = quizData.results;
-    renderQuestion(quizData, 0, questionField);
-    let answers = createAnswersArray(quizData, 0);
+    renderQuestion(quizData, currentQuestionIndex, questionField);
+    let answers = createAnswersArray(quizData, currentQuestionIndex);
     renderAnswers(answers, answersField);
 }
 
 
 function createAnswersArray(data, index) {
-    incorrectAnswers = data[index].incorrect_answers;
+    let answers = data[index].incorrect_answers;
     correctAnswer = data[index].correct_answer;
-    incorrectAnswers.push(correctAnswer);
-    return incorrectAnswers;
+    answers.push(correctAnswer);
+    shuffle(answers);
+    return answers;
 }
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    while (currentIndex != 0) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
+
 function renderQuestion(data, index, field) {
     field.innerHTML = data[index].question;
 }
@@ -50,7 +69,35 @@ function renderAnswers(answersArray, field) {
         buttons.innerHTML=answersArray[i];
         buttons.classList.add('answer')
         field.appendChild(buttons);
+        buttons.addEventListener('click', checkAnswer)
       }
+}
+async function getNextQuestion() {
+    let quizData = await getQuizData();
+    quizData = quizData.results;
+    if (currentQuestionIndex >= quizData.length) {
+        quiz.style.display = "none";
+        resultsScreen.style.display = "block";
+        resultsScreen.innerHTML = `Nice! Your got ${rightAnswersCount} of the ${quizData.length} questions right.`;
+    } else {
+    correctAnswer = quizData[currentQuestionIndex].correct_answer;
+    answersField.innerHTML = "";
+    renderQuestion(quizData, currentQuestionIndex, questionField);
+    let answers = createAnswersArray(quizData, currentQuestionIndex);
+    renderAnswers(answers, answersField);
+    }
+    currentQuestionIndex++;
+}
+
+function checkAnswer(){
+    if (this.innerHTML == correctAnswer) {
+        this.style.background = 'green';
+        rightAnswersCount++;
+        getNextQuestion();
+    } else {
+        this.style.background = 'red';
+        getNextQuestion();
+    }
 }
 
 renderQuiz();
